c54112c6f06e479db65892b2d809552515f81f75
Jason Scot <jason.s.scot@gmail.com>
2019-02-05 17:17:04 -0500

    outlined partners page

--- public/js/main.js
+++ public/js/main.js
@@ -9,7 +9,7 @@ $(document).ready(function () {
     var path_file_base = "modules/";
 
     // Loads the homepage carousel module
-    var carousel = document.getElementById('myCarousel');
+    var carousel = document.getElementById('carousel-module');
     if (carousel != null) {
         var path_file_appended = path_file_base + "home/";
         path_file_appended += "carousel/";
@@ -17,9 +17,9 @@ $(document).ready(function () {
         var appendage = "";
         var _path_file = path_file_appended + "carousel.json";
         retrieveJSON(_path_file, function (elements) {
-            appendage += '<ol class="carousel-indicators">&nbsp;<li data-target="#myCarousel" data-slide-to="0" class="active"></li>&nbsp;';
+            appendage += '<ol class="carousel-indicators">&nbsp;<li data-target="#carousel-module" data-slide-to="0" class="active"></li>&nbsp;';
             for (var _i = 1; _i < elements.length; _i++) {
-                appendage += '<li data-target="#myCarousel" data-slide-to="' + _i + '"></li>&nbsp;';
+                appendage += '<li data-target="#carousel-module" data-slide-to="' + _i + '"></li>&nbsp;';
             }
             appendage += '</ol>';
 
@@ -34,14 +34,14 @@ $(document).ready(function () {
                 //appendage += '<img src="' + path_file_appended + elements[i].img + '" alt="' + elements[i].name + '" width="1200" height="700"><div class="carousel-caption"><p>' + elements[i].desc + '</p></div></div>';
             }
             appendage += '</div>';
-            appendage += '<a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev"><span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span><span class="sr-only">Previous</span></a><a class="right carousel-control" href="#myCarousel" role="button" data-slide="next"><span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span><span class="sr-only">Next</span></a>';
+            appendage += '<a class="left carousel-control" href="#carousel-module" role="button" data-slide="prev"><span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span><span class="sr-only">Previous</span></a><a class="right carousel-control" href="#carousel-module" role="button" data-slide="next"><span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span><span class="sr-only">Next</span></a>';
 
             carousel.innerHTML += appendage;
         });
     }
 
     // Loads the members page modules
-    var members = document.getElementById('meetings');
+    var members = document.getElementById('meetings-module');
     if (members != null) {
         var path_file_appended_base = path_file_base + "members/";
         var _path_file_appended = path_file_appended_base + "meetings/";
@@ -52,13 +52,13 @@ $(document).ready(function () {
         retrieveJSON(path_file, function (elements) {
             var element = elements[0];
 
-            appendage_members += '<div class="row"><div class="col-sm-1"></div><div class="col-sm-10 banner"><h2>Meeting Details: ' + element.season + ' ' + element.year + '</h2><hr><br><div class="row"><div class="col-sm-1"></div><div class="col-sm-4"><em>Time</em><br><h3>' + element.time + ' ' + element.ampm + '</h3></div><div class="col-sm-2"><i class="fas fa-meteor space-down-mobile" style="font-size: 60px;"></i></div><div class="col-sm-4"><em>Place</em><br><h3><a href="' + element.bldglink + '" target="_blank">' + element.bldgabbreviation + '</a>  ' + element.room + '</h3></div><div class="col-sm-1"></div></div><div class="row"><div class="col-sm-1"></div><div class="col-sm-10"><em>' + element.pattern + '</em><br><h3><a href="' + element.pdflink + '" target="_blank">List of Dates</a></h3></div><div class="col-sm-1"></div></div><br><div class="row smaller"><div class="col-sm-1"></div><div class="col-sm-10"><p><em>For the most up-to-date meeting information, including topics, room changes, and cancellations, make sure you are on our mailing list.</em></p><p>Send all meeting-related inquiries to <a href="mailto:' + element.contactemail + '">' + element.contactemail + '</a></p></div><div class="col-sm-1"></div></div></div><div class="col-sm-1"></div></div>';
+            appendage_members += '<div class="row"><div class="col-sm-1"></div><div class="col-sm-10 banner"><h2>Meeting Details: ' + element.season + ' ' + element.year + '</h2><hr><br><div class="row"><div class="col-sm-1"></div><div class="col-sm-4"><em>Time</em><br><h3>' + element.time + ' ' + element.ampm + '</h3></div><div class="col-sm-2"><i class="fas fa-meteor space-down-mobile" style="font-size: 60px;"></i></div><div class="col-sm-4"><em>Place</em><br><h3><a href="' + element.bldglink + '" target="_blank">' + element.bldgabbreviation + '</a>  ' + element.room + '</h3></div><div class="col-sm-1"></div></div><div class="row"><div class="col-sm-1"></div><div class="col-sm-10"><em>' + element.pattern + '</em><br><h3><a href="' + element.pdflink + '" target="_blank">List of Dates</a></h3></div><div class="col-sm-1"></div></div><br><div class="row smaller"><div class="col-sm-1"></div><div class="col-sm-10"><p><em>For the most up-to-date meeting information, including topics, room changes, and cancellations, make sure you are subscribed to our mailing list.</em></p><p>Send all meeting-related inquiries to <a href="mailto:' + element.contactemail + '">' + element.contactemail + '</a></p></div><div class="col-sm-1"></div></div></div><div class="col-sm-1"></div></div>';
 
             members.innerHTML += appendage_members;
         });
 
         // Loads the eboard module
-        var eboard = document.getElementById('eboard');
+        var eboard = document.getElementById('eboard-module');
         if (eboard != null) {
             _path_file_appended = path_file_appended_base + "eboard/";
             var _appendage = "";
